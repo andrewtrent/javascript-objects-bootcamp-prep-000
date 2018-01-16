a18c8196abac839c68f1511564c8cf0def5e9f7a
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-16 21:46:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
 var playlist = {'Kanye West':"Black Skinhead"};
 function updatePlaylist(playlistObject,artist,title){
   Object.assign(playlistObject, {artist: title})
+  return playlistObject
+}
+function removeFromPlaylist(playlistObject, artist){
+  
 }
